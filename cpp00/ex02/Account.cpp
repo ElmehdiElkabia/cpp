41d3065e87d7faed68bf971882d819dc9d5017e0
elmehdi elkabia <elmehdielkabia@gmail.com>
2025-09-19 11:48:58 +0100

    refactor: update Makefile to use CPPFLAGS consistently and add Account class implementation

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,135 @@
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";created"
+			  << std::endl;
+}
+
+Account::~Account()
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";closed"
+			  << std::endl;
+}
+
+int Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts
+			  << ";total:" << _totalAmount
+			  << ";deposits:" << _totalNbDeposits
+			  << ";withdrawals:" << _totalNbWithdrawals
+			  << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	_amount += deposit;
+	_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";p_amount:" << _amount - deposit
+			  << ";deposit:" << deposit
+			  << ";amount:" << _amount
+			  << ";nb_deposits:" << _nbDeposits
+			  << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	if (_amount >= withdrawal)
+	{
+		_amount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		_totalAmount -= withdrawal;
+
+		_displayTimestamp();
+		std::cout << "index:" << _accountIndex
+				  << ";p_amount:" << _amount + withdrawal
+				  << ";withdrawal:" << withdrawal
+				  << ";amount:" << _amount
+				  << ";nb_withdrawals:" << _nbWithdrawals
+				  << std::endl;
+		return true;
+	}
+	else
+	{
+		_displayTimestamp();
+		std::cout << "index:" << _accountIndex
+				  << ";p_amount:" << _amount
+				  << ";withdrawal:" << "refused"
+				  << std::endl;
+		return false;
+	}
+}
+
+int Account::checkAmount(void) const
+{
+	return _amount;
+}
+
+void Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			  << ";amount:" << _amount
+			  << ";deposits:" << _nbDeposits
+			  << ";withdrawals:" << _nbWithdrawals
+			  << std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+
+	std::time_t t = std::time(NULL);
+	std::tm *datetime = std::localtime(&t);
+	char	output[50];
+
+	std::strftime(output, sizeof(output), "[%Y%m%d_%H%M%S] ", datetime);
+	std::cout << output;
+}
