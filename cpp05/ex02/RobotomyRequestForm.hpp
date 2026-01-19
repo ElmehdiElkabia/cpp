d6329346f84a87481ce9c835fb7770f3f38c172b
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-01-19 13:14:49 +0100

    init the class RobotomyRequestForm

---
+++ cpp05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,17 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm
+{
+public:
+	RobotomyRequestForm();
+	RobotomyRequestForm(const RobotomyRequestForm &copy);
+	RobotomyRequestForm &operator=(const RobotomyRequestForm &copy);
+	~RobotomyRequestForm();
+
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
