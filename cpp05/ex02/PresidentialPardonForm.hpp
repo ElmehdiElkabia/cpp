b9caf10dc3d12fd204339e96ec233fecf37d3288
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-01-19 13:14:24 +0100

    init the class PresidentialPardonForm

---
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,17 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+public:
+	PresidentialPardonForm();
+	PresidentialPardonForm(const PresidentialPardonForm &copy);
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &copy);
+	~PresidentialPardonForm();
+
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
