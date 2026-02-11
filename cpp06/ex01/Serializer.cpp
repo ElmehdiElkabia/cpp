cdc549ebe74395691a791ffebc36777af60ca0c2
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-11 16:59:40 +0100

    Refactor serialize and deserialize methods in Serialize class for improved clarity

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -14,10 +14,10 @@ Serialize::~Serialize() {};
 
 uintptr_t Serialize::serialize(Data *ptr)
 {
-
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *Serialize::deserialize(uintptr_t raw)
 {
-	
+	return reinterpret_cast<Data *>(raw);
 }
