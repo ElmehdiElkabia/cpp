2b4408c15c7debf7c94321026c5f7a9f490a4b69
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-11 12:16:05 +0100

    Add Data class implementation with constructors, destructor, and accessors

---
+++ cpp06/ex01/Data.cpp
@@ -0,0 +1,27 @@
+#include "Data.hpp"
+
+Data::Data() : str("str"), number(0) {};
+
+Data::Data(std::string t, int n) : str(t), number(n) {};
+
+Data::Data(const Data &copy)
+{
+	this->str = copy.str;
+	this->number = copy.number;
+}
+
+Data &Data::operator=(const Data &copy)
+{
+	if (this != &copy)
+	{
+		this->str = copy.str;
+		this->number = copy.number;
+	}
+	return *this;
+}
+
+Data::~Data() {}
+
+int Data::getInt() const { return number; }
+
+std::string Data::getString() const { return str; }
