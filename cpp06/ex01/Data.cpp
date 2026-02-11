8306627c13794ec84a51ec70002db4e21cc4799f
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-11 12:17:25 +0100

    Refactor copy constructor in Data class for improved clarity and conciseness

--- cpp06/ex01/Data.cpp
+++ cpp06/ex01/Data.cpp
@@ -4,11 +4,7 @@ Data::Data() : str("str"), number(0) {};
 
 Data::Data(std::string t, int n) : str(t), number(n) {};
 
-Data::Data(const Data &copy)
-{
-	this->str = copy.str;
-	this->number = copy.number;
-}
+Data::Data(const Data &copy) : str(copy.str), number(copy.number) {};
 
 Data &Data::operator=(const Data &copy)
 {
