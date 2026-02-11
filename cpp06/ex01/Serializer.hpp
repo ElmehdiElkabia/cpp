b3a3330f10ff07ddd826b4e8a8c4d43777309c99
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-11 11:52:21 +0100

    Enhance Serializer class with private constructors and public serialization methods

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -3,12 +3,17 @@
 
 #include <iostream>
 
-
-
 class Serialize
 {
-	
-}
+private:
+	Serialize();
+	Serialize(const Serialize &copy);
+	Serialize &operator=(const Serialize &copy);
+	~Serialize();
 
+public:
+	static uintptr_t serialize(Data *ptr);
+	Data *deserialize(uintptr_t raw);
+};
 
 #endif
