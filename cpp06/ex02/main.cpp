d8fe89e687856efe69a5b302f8f9a06de579da89
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-11 18:39:53 +0100

    Add identify function to determine the type of Base class instances

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -14,3 +14,13 @@ Base *generate(void)
 	else if (i == 2)
 		return new C();
 }
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+}
