b807ffafc84f2976203c36261e2c79bc980ce4cf
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-02-22 17:25:03 +0000

    Refactor Array class to encapsulate member variables and add exception handling for index access

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,17 +2,36 @@
 #define ARRAY_HPP
 
 #include <iostream>
+#include <exception>
+#include <cstdlib>
+#include <ctime>
+
 
 template <typename T>
 class Array
 {
-	public:
+private:
+	T *_array;
+	unsigned int _size;
+
+public:
 	Array();
 	Array(unsigned int n);
+	Array(const Array &copy);
+	T &operator=(const Array &copy);
+	~Array();
+
+	unsigned int size() const;
+
+	T &operator[](unsigned int i);
+	const T &operator[](unsigned int i) const;
+
+	class IndexNotFound : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
 };
 
-
-
-
+#include "Array.tpp"
 
 #endif
