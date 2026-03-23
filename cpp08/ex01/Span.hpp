e638afda916f0c3810909a71c4d6c22750a5b9ad
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-03-23 19:54:22 +0100

    Add Span class definition with methods for adding numbers and calculating spans

---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,40 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <algorithm>
+
+class Span
+{
+private:
+	unsigned int n;
+	std::vector<int> v;
+
+public:
+	Span();
+	Span(unsigned int i);
+	Span(const Span &copy);
+	Span &operator=(const Span &copy);
+	~Span();
+
+	void addNumber(int a);
+
+	int shortestSpan();
+	int longestSpan();
+
+
+
+	class ContainerIsFull : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+
+		class NotEnough : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+};
+
+#endif
