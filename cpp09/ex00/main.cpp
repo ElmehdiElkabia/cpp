57d26280bc617678af26211b015265fc53bb587b
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-04-13 20:58:20 +0100

    Add main function to initialize BitcoinExchange and load database

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,26 @@
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./btc input.txt" << std::endl;
+		return (1);
+	}
+
+	// try
+	// {
+		BitcoinExchange btc;
+
+		btc.loadDatabase("data.csv");
+		btc.printDatabase();
+		// btc.processInput(argv[1]);
+
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
+	return 0;
+}
