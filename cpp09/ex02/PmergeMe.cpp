eb1aae735e6971798f7504f12b8fb4cc4000bb92
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-04-21 17:21:34 +0100

    Implement parseInput method in PmergeMe class for input validation and storage

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -19,3 +19,25 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &copy)
 
 PmergeMe::~PmergeMe() {}
 
+
+void PmergeMe::parseInput(char **argv)
+{
+    std::string token;
+    for (int i = 1; argv[i] != nullptr; ++i)
+    {
+        token = argv[i];
+        if (!isValidInput(token))
+        {
+            std::cerr << "Error: Invalid input '" << token << "'. Only positive integers are allowed." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        int value = toInt(token);
+        if (hasDuplicate(value))
+        {
+            std::cerr << "Error: Duplicate value '" << value << "' found." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        c_vector.push_back(value);
+        c_deque.push_back(value);
+    }
+}
